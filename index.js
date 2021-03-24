5d2a04df8c26e6cb4983e247b3ce87afc3f3173c
David Medina Mena <davidmedinamena@gmail.com>
2021-03-24 12:28:00 -0400

    firts-commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 80;
+const port = process.env.PORT || 80;
 
 const scrapWebPage = async (url) => {
   const browser = await puppeter.launch();
